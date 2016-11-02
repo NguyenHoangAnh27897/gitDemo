3e8973f63c7f1188eac050eb862e9f54db757227
hOaNgAnH <hoanganh27897@gmail.com>
2016-11-02 17:34:59 +0700

    Change

--- hOaNgAnH/src/aNh.java
+++ hOaNgAnH/src/aNh.java
@@ -1,11 +1,16 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
 public class aNh extends JFrame {
+	JButton btnDe = new JButton("hOaNgAnHdEpTrAi");
 		public aNh(){
 			setSize(400,400);
 			setTitle("hOaNgAnH");
 			setLayout(null);
+			
+			this.add(btnDe);
+			btnDe.setBounds(20, 20, 150, 35);
 		}
 	/**
 	 * @param args
