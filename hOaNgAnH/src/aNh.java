500aefbaa35f700ea9e1fe7576f30aa39085191b
hOaNgAnH <hoanganh27897@gmail.com>
2016-11-02 17:20:21 +0700

    Init project

---
+++ hOaNgAnH/src/aNh.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+
+public class aNh extends JFrame {
+		public aNh(){
+			setSize(400,400);
+			setTitle("hOaNgAnH");
+			setLayout(null);
+		}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		aNh main = new aNh();
+		main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		main.setVisible(true);
+	}
+
+}
